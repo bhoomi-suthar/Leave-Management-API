0d0429547a99bcebb6b325e211d634eabe40359c
Bhoomi Suthar <sutharbhoomi1007@gmail.com>
2026-04-04 16:17:05 +0530

    Initial commit

--- auth.py
+++ auth.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from models import User, Leave
 from database import get_db, pwd_context, create_token, get_current_user
+import hashlib
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -11,11 +12,11 @@ templates = Jinja2Templates(directory="templates")
 
 @router.post("/signup")
 def signup(
-    request:  Request,
+    request: Request,
     username: str = Form(...),
     password: str = Form(...),
-    role:     str = Form(...),
-    db: Session   = Depends(get_db)
+    role: str = Form(...),
+    db: Session = Depends(get_db)
 ):
     existing_user = db.query(User).filter(User.username == username).first()
     if existing_user:
@@ -23,7 +24,8 @@ def signup(
             request=request, name="signup.html",
             context={"error": "Username already exists"}
         )
-    new_user = User(username=username, password=pwd_context.hash(password), role=role)
+    hashed = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    new_user = User(username=username, password=hashed, role=role)
     db.add(new_user)
     db.commit()
     return RedirectResponse(url="/login-page", status_code=303)
@@ -31,23 +33,23 @@ def signup(
 
 @router.post("/login")
 def login(
-    request:  Request,
+    request: Request,
     username: str = Form(...),
-    password: str = Form(...),
-    db: Session   = Depends(get_db)
+    password: str = Form(...), 
+    db: Session = Depends(get_db)
 ):
     db_user = db.query(User).filter(User.username == username).first()
-    if not db_user or not pwd_context.verify(password, db_user.password):
+    if not db_user or db_user.password != password:  
         return templates.TemplateResponse(
             request=request, name="login.html",
             context={"error": "Invalid credentials"}
         )
+    
     token = create_token({"username": db_user.username, "role": db_user.role})
     return templates.TemplateResponse(
         request=request, name="token.html",
         context={"token": token, "role": db_user.role, "username": db_user.username}
     )
-
 
 @router.post("/enter-token")
 def enter_token(request: Request, token: str = Form(...), db: Session = Depends(get_db)):
